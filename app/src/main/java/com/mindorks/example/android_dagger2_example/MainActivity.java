125dd94054176b118b589fc857bfefc3357db629
janishar <janishar.ali@gmail.com>
2016-12-25 01:39:33 +0530

    project creation

---
+++ app/src/main/java/com/mindorks/example/android_dagger2_example/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mindorks.example.android_dagger2_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
